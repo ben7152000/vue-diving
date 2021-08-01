3dc20741432896ed3fe270f20319bc8905f1ce0c
Ben Chou <ben7152000@gmail.com>
2021-08-01 13:13:07 +0800

    app layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div id="app" class="main-container">
+    <main>
+      <router-view/>
+    </main>
   </div>
 </template>
 
+<script>
+export default {
+
+}
+</script>
+
 <style lang="scss">
+@import "scss/main";
+
+main {
+  margin-top: 60px;
+  flex: 1;
+}
+
+.main-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
 
 </style>
