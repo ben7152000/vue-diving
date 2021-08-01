2c6e52e76f21ea0a5d91afefc65066c13dc2984e
Ben Chou <ben7152000@gmail.com>
2021-08-01 12:50:41 +0800

    project init

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+// element ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import lang from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale'
+
+Vue.use(ElementUI)
+
+// lang
+locale.use(lang)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
