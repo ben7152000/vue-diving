f70006dfa25d839eb664b851184a02d800ba46a2
Ben Chou <ben7152000@gmail.com>
2021-08-01 13:09:00 +0800

    not fount views create

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    NotFound
+  <div class="container">
+    <p>404 找不到網頁</p>
   </div>
 </template>
 
@@ -11,5 +11,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.container {
+  width: 100%;
+  height: 50vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(to bottom, rgba(white, 0.8), rgba(white, 0));
+}
 
+p {
+  font-weight: 500;
+  letter-spacing: 2px;
+  color: #44607a;
+}
 </style>
